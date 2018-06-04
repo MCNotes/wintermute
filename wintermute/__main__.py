5c312fcccfd8b8edad688323e2be85cc9c2f80be
Tania Allard <taniar.allard@gmail.com>
2018-06-04 15:44:17 +0100

    Add multiple events

--- wintermute/__main__.py
+++ wintermute/__main__.py
@@ -1,4 +1,5 @@
 import os
+
 import aiohttp
 
 from aiohttp import web
@@ -10,26 +11,55 @@ router = routing.Router()
 
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    """
-    Whenever an issue is opened, greet the author and say thanks.
-    """
-    url = event.data["issue"]["comments_url"]
-    author = event.data["issue"]["user"]["login"]
+    """ Whenever an issue is opened, greet the author and say thanks."""
+    url = event.data['issue']['comments_url']
+    user = event.data['issue']['user']['login']
+    message = f"Thanks for opening the issue @{user}, will look into it (I'm a bot 🤖)"
+    await gh.post(url, data={
+        'body': message,
+    })
 
-    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
-    await gh.post(url, data={"body": message})
+@router.register("pull_request", action="closed")
+async def pr_closed_event(event, gh, *args, **kwargs):
+    """When a PR has been closed, say thanks"""
+    user = event.data["pull_request"]["user"]["login"]
+    is_merged = event.data["pull_request"]["merged"]
+    url = event.data["pull_request"]["comments_url"]
+    if is_merged:
+        message = f"Thanks for the PR @{user}"
+        await gh.post(url, data={"body": message})
+
+@router.register("issue_comment", action="created")
+async def issue_comment_created_event(event, gh, *args, **kwargs):
+    """Thumbs up for my own issue comment"""
+    url = f"{event.data['comment']['url']}/reactions"
+    user = event.data["comment"]["user"]["login"]
+    if user == "trallard":
+        await gh.post(url,
+                      data={'content': '+1'},
+                      accept="application/vnd.github.squirrel-girl-preview+json")
+
 
 async def main(request):
+    # read the GitHub webhook payload
     body = await request.read()
 
+    # our authentication token and secret
     secret = os.environ.get("GH_SECRET")
     oauth_token = os.environ.get("GH_AUTH")
 
+    # a representation of GitHub webhook event
     event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    # instead of mariatta, use your own username
     async with aiohttp.ClientSession() as session:
         gh = gh_aiohttp.GitHubAPI(session, "trallard",
                                   oauth_token=oauth_token)
+
+        # call the appropriate callback for the event
         await router.dispatch(event, gh)
+
+    # return a "Success"
     return web.Response(status=200)
 
 
